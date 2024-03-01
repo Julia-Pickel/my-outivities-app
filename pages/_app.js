45aaed9e5918313470c967e1ff5b9ab8107ccfe1
Julia Pickel <juliapickel@gmx.de>
2024-03-01 18:03:18 +0100

    import SessionProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,30 @@
 import { SWRConfig } from "swr";
 import GlobalStyle from "../styles";
 import Layout from "@/components/Layout";
+import { SessionProvider } from "next-auth/react";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+    <SessionProvider session={pageProps.session}>
       <SWRConfig
         value={{
-          fetcher,
+          fetcher: async (...args) => {
+            const response = await fetch(...args);
+            if (!response.ok) {
+              throw new Error(`Request with ${JSON.stringify(args)} failed.`);
+            }
+            return await response.json();
+          },
         }}
       >
         <GlobalStyle />
         <Component {...pageProps} />
         <Layout />
       </SWRConfig>
+      </SessionProvider>
     </>
   );
 }
